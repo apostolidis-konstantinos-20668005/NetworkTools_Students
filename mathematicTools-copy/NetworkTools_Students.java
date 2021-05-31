cb2c684d500daa2757ccf05eecd6260a6c228016
apostolidis-konstantinos-20668005 <80473795+apostolidis-konstantinos-20668005@users.noreply.github.com>
2021-05-31 14:19:57 +0300

    Add files via upload

--- mathematicTools-copy/NetworkTools_Students.java
+++ mathematicTools-copy/NetworkTools_Students.java
@@ -14,55 +14,36 @@ public class NetworkTools_Students
     private String[] ip_array;
     private String[] mask_array;
 
-/**
- * Κατασευαστής αντικειμένων της κλάσης NetworkTools
- */
-public NetworkTools_Students ()
-{
-    // Παράδειγμα ανάθεσης - αρχικοποίηση μεταβλητών
-    myIP = "192.168.1.130";
-    myMask = "255.255.255.192";
-     
-    //Διαχωρισμός των octets μιας διεύθυνσης ΙΡ μετρώντας ένα-ένα τα στοιχεία της. 
-    ip_array = new  String[4];
-    char c1;
-    int pos1=0;
-    String octet1="";
-     
-    for (int i=0; i< myIP.lenght(); i++)
-    {
-        c1=myIP.charAt(i);
-       if (c1 == '.')
-       {
-          ip_array[pos1] = octet1;
-          pos2++;
-          octet =""; 
-       }
-       else
-       {
-          octet1 = octet1 + String.value0f(c1);
-       }
-    }
-   ip_array[3]=octet1;
 
-   //Διαχωρισμός των octets μιας μάσκας μετρώντας ένα-ένα τα στοιχεία της
-   mask_array = new String[4];
-   char c2;
-   int pos2 = 0;
-   String octet2="";
-   for (int i=0; i< myMask.length(); i++)
-   {
-     c2=myMask.charAt(i);
-      if (c2 == '.')
-     {
-         mask_array[pos2] = octet2;
-         pos2++;
-         octet2 = "";
-     }  
-     else
-     {
-          octet2 = octet2 + String.value0f(c2);
-     }     
-   }
-   mask_array[3]=octet2;
+/**
+* Eπιτρέπει την αλλαγή της IP
+*/
+public void setIP (String newIP)
+{ 
+    this.myIP = newIP;           //εκχωρούμε μια νέα διεύθυνση
+    
+    /* Τask2a: Mε την αλλαγή της ΙΡ θα πρέπει να επικαιροποιηθεί και ο 
+     * βοηθητικός πίνακας octets
+     * Bοήθεια: Aντικαταστήστε τη τιμή null με την συνάρτηση stringToArray
+     * η την a_string.split
+     */
+    this.ip_array = null;
 }
+/**
+* Eπιτρέπει την αλλαγή της Mask
+*/
+public void setMask (String newMask)
+{ 
+    this.myMask = newMask;           //εκχωρούμε μια νέα μάσκα
+    
+    /* Τask2a: Mε την αλλαγή της μάσκας θα πρέπει να επικαιροποιηθεί και ο 
+     * βοηθητικός πίνακας octets
+     * Bοήθεια: Aντικαταστήστε τη τιμή null με την συνάρτηση stringToArray
+     * η την a_string.split
+     */
+    this.Mask_array = null;
+
+
+    
+    
+}    
